5a4e55bb3922ec7c4c2dfd6c24623b88e2979dc8
松川信之 <theviers@gmail.com>
2024-08-08 23:52:59 +0900

    プロジェクト作成

---
+++ webapp/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO todos(todo,detail)
+VALUES
+('買い物','スーパーで食材を購入する');
+
+INSERT INTO todos(todo,detail)
+VALUES('図書館に行く','本を借りる');
+
+INSERT INTO todos(todo,detail)
+VALUES('ジムに行く','運動する');
